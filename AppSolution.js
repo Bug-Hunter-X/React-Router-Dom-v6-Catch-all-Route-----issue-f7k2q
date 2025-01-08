b8675f200091dc80343a4001781bb6888bce42ae
BugHunter-X <askzenai@gmail.com>
2025-01-08 04:52:06 +0100

    Add AppSolution.js file

---
+++ AppSolution.js
@@ -0,0 +1,33 @@
+```javascript
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function NoMatch() {
+  let location = useLocation();
+  return (
+    <h1>
+      404: No match for <code>{location.pathname}</code>
+    </h1>
+  );
+}
+export default App;
+```
